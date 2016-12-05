7b1c64ce7ea0738a3d75a76aabe8bb6a1936b93f
Shota Hoshino <s-hoshino@quartetcom.co.jp>
2016-12-05 16:29:29 +0900

    Move quartet/stripe-bundle to peer-dependency

--- src/DependencyInjection/QuartetPaymentStripeExtension.php
+++ src/DependencyInjection/QuartetPaymentStripeExtension.php
@@ -4,8 +4,10 @@
 namespace Quartet\Payment\StripeBundle\DependencyInjection;
 
 
+use Quartet\Stripe\Stripe;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Exception\LogicException;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\DependencyInjection\Reference;
@@ -19,6 +21,10 @@ class QuartetPaymentStripeExtension extends Extension
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
+
+        if (!class_exists(Stripe::class)) {
+            throw new LogicException(sprintf('Bundle cannot be loaded as the %s is not found. Please install quartet/stripe-bundle.', Stripe::class));
+        }
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('service.yml');
